7ff92808e07e7f7191af0e37f89d98ff0685b4d6
Amir Khan <amir.khan@salesforce.com>
2024-06-24 19:45:18 +0200

    J17 support added

--- src/main/java/org/mule/extension/mulechain/internal/AwsbedrockExtension.java
+++ src/main/java/org/mule/extension/mulechain/internal/AwsbedrockExtension.java
@@ -3,7 +3,10 @@ package org.mule.extension.mulechain.internal;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
-
+import org.mule.sdk.api.annotation.JavaVersionSupport;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_11;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_17;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
 
 /**
  * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
@@ -12,6 +15,7 @@ import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 @Xml(prefix = "aws-bedrock")
 @Extension(name = "Amazon Bedrock")
 @Configurations(AwsbedrockConfiguration.class)
+@JavaVersionSupport({JAVA_8, JAVA_11, JAVA_17})
 public class AwsbedrockExtension {
 
 }
