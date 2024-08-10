f19d11ac42f603ce673b9827680d9d29433744d7
Amir Khan <amir.khan@salesforce.com>
2024-08-10 18:15:52 +0200

    update

--- src/main/java/org/mule/extension/mulechain/internal/AwsbedrockConfiguration.java
+++ src/main/java/org/mule/extension/mulechain/internal/AwsbedrockConfiguration.java
@@ -13,7 +13,7 @@ import org.mule.runtime.extension.api.annotation.Configuration;
  * This class represents an extension configuration, values set in this class are commonly used across multiple
  * operations since they represent something core from the extension.
  */
-@Configuration(name="aws-bedrock-configuration") 
+@Configuration(name="Config") 
 @Operations({
           AwsbedrockOperations.class, 
           AwsbedrockImageModelOperations.class,
